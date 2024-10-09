0f529998afd667033a0f97117a3ae8d35238491b
lixz <lixz@dns.com>
2024-10-09 10:19:10 +0800

    feat(release): v1.10.1

--- cmd/sshs/main.go
+++ cmd/sshs/main.go
@@ -13,7 +13,7 @@ func main() {
 		Name:      "sshs",
 		Usage:     "make ssh scp easy",
 		UsageText: "sshs [flags] [command] [args...]",
-		Version:   "1.10.0",
+		Version:   "1.10.1",
 		Action:    TerminalAction,
 		Commands: []*cli.Command{
 			{
